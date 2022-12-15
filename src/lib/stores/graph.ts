c3bd9b36bc4455545372142e5bf5064b5108a673
Albert <albert@framework.tools>
2022-12-15 17:17:17 +1030

    remove https part, update env to include it

--- src/lib/stores/graph.ts
+++ src/lib/stores/graph.ts
@@ -17,7 +17,7 @@ export const graph_init = (auth_token: string | null, api_domain: string): Graph
                 const BACKENDAPI = api_domain  // fallback to rust test http main.rs server
                 const auth_token = this.auth_token
 
-                const request = new Request(`//${BACKENDAPI}/graph`, {
+                const request = new Request(`${BACKENDAPI}/graph`, {
                     method: "POST",
                     body: JSON.stringify({
                         query,
