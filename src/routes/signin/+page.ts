3702066157eda04d4a88cfb085260819752ade9f
Albert <albert@framework.tools>
2023-01-20 22:09:15 +1030

    Change to hostname to ignore port

--- src/routes/signin/+page.ts
+++ src/routes/signin/+page.ts
@@ -2,13 +2,15 @@ import type { PageLoad } from "./$types"
 import { error, redirect } from "@sveltejs/kit"
 
 const allowed_hosts = ["localhost", "light-university.vercel.app"]
+
 export const load: PageLoad = async function load({ parent, url }) {
     const data = await parent()
 
     let redirect_param = url.searchParams.get("redirect")
     if(redirect_param) {
-        const redirect_url = new URL(decodeURIComponent(redirect_param))
-        if (!allowed_hosts.includes(redirect_url.host)) {
+        console.log(redirect_param)
+        const redirect_url = new URL(redirect_param)
+        if (!allowed_hosts.includes(redirect_url.hostname)) {
             throw error(400, {
                 message: "Host redirect is not allowed",
                 code: "INVALID_REDIRECT",
