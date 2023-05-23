d1e4f0bfc9e9a516920e1e9c79825b4893b76e7c
Albert <albert@framework.tools>
2023-05-23 21:04:21 +0930

    Improve session handling code for invalid tokens

--- src/lib/api/user.ts
+++ src/lib/api/user.ts
@@ -2,20 +2,20 @@ import { MeDocument, type MeQuery } from "$lib/graphql/graphql-types"
 import type { AlertsStore } from "$lib/stores/alerts"
 import type { GraphClient } from "$lib/stores/graphql"
 import { MessageType } from "$lib/types/message"
-
-export class InvalidTokenError extends Error {
-    constructor() {
-        super("InvalidTokenError")
-    }
-}
+import { error as svelte_error } from "@sveltejs/kit"
 
 export async function get_me(graph: GraphClient, alerts: AlertsStore): Promise<MeQuery["me"] | null> {
-    const { data, error  } = await graph.gquery(MeDocument, {})
+    const { data, error } = await graph.gquery(MeDocument, {})
 
     // find any errors that are related to invalid auth token
-    if(error?.graphQLErrors.find(err => err?.extensions?.code === "INVALID_TOKEN") !== undefined) {
+    const err = error?.graphQLErrors.find(err => err?.extensions?.code === "INVALID_TOKEN")
+
+    if(err !== undefined) {
         alerts.create_alert(MessageType.Info, "Your session has expired. Please sign in again.")
-        throw new InvalidTokenError()
+        throw svelte_error(401, {
+            message: err.message,
+            code: err.extensions?.code as string,
+        })
     }
 
     if (error) {
