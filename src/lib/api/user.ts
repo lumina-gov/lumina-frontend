63dbc081b9a89b87f871688615c5ab5e418cea06
Albert <albert@framework.tools>
2023-05-23 22:04:36 +0930

    Invalid token logic

--- src/lib/api/user.ts
+++ src/lib/api/user.ts
@@ -2,7 +2,6 @@ import { MeDocument, type MeQuery } from "$lib/graphql/graphql-types"
 import type { AlertsStore } from "$lib/stores/alerts"
 import type { GraphClient } from "$lib/stores/graphql"
 import { MessageType } from "$lib/types/message"
-import { error as svelte_error } from "@sveltejs/kit"
 
 export async function get_me(graph: GraphClient, alerts: AlertsStore): Promise<MeQuery["me"] | null> {
     const { data, error } = await graph.gquery(MeDocument, {})
@@ -12,10 +11,7 @@ export async function get_me(graph: GraphClient, alerts: AlertsStore): Promise<M
 
     if(err !== undefined) {
         alerts.create_alert(MessageType.Info, "Your session has expired. Please sign in again.")
-        throw svelte_error(401, {
-            message: err.message,
-            code: err.extensions?.code as string,
-        })
+        throw err
     }
 
     if (error) {
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,6 +4,7 @@ import { get_me } from "$lib/api/user"
 import { browser } from "$app/environment"
 import { user_store_init } from "$lib/stores/user_store"
 import { set_cookie } from "$lib/utils/cookie"
+import type { GraphQLError } from "graphql"
 
 export async function load ({ data: { auth_token }}) {
     const user_store = user_store_init(auth_token)
@@ -13,7 +14,7 @@ export async function load ({ data: { auth_token }}) {
     try {
         user_store.user = await get_me(graph, alerts)
     } catch (e) {
-        if (browser && e && typeof e === "object" && "code" in e && e.code === "INVALID_TOKEN") {
+        if (browser && (e as GraphQLError)?.extensions?.code == "INVALID_TOKEN") {
             set_cookie("token", null)
         }
         user_store.auth_token = null
