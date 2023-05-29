056ceb1b1523b9cdd5dc6bd621af1b8c100cfa7d
Albert <albert@lumina.earth>
2023-05-30 01:38:20 +0930

    Mobile optimise

--- src/routes/(site)/milestones/MilestoneItem.svelte
+++ src/routes/(site)/milestones/MilestoneItem.svelte
@@ -13,12 +13,12 @@ export let last: boolean
 </script>
 
 <div class="milestone">
-    <VerticalLayout align_items={"flex-end"}>
+    <div class="dates">
         <MilestoneDate date={milestone.date}></MilestoneDate>
         {#each milestone.tags as tag}
             <Tag>{ tag }</Tag>
         {/each}
-    </VerticalLayout>
+    </div>
     <div
         class="line"
         class:last-line={ last }>
@@ -61,12 +61,17 @@ export let last: boolean
     max-width:  850px
     grid-template-columns: 150px max-content 2fr;
     grid-gap 0 24px
+    @media (max-width $tablet)
+        grid-template-columns max-content 1fr
+        grid-gap 12px 24px
 .line
     width 4px
     height 100%
     display flex
     justify-content center
     background transparify(white, 10%)
+    @media (max-width $tablet)
+        grid-row 1 / span 2
 .last-line
     background none
 .circle
@@ -79,5 +84,15 @@ export let last: boolean
     display flex
     flex-direction column
     gap 6px
+
+.dates
+    display flex
+    flex-direction column
+    text-align right
+    align-items end
+    gap 6px
+    @media (max-width $tablet)
+        align-items start
+        text-align left
 
 </style>
